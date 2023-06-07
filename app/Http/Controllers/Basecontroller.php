40217e4f8705ae44828f18aded379d57e871f202
Vishal Bhalerao <bhaleraov180@gmail.com>
2023-06-07 17:00:16 +0530

    changes done

---
+++ app/Http/Controllers/Basecontroller.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Basecontroller extends Controller
+{
+    public function home()
+{
+    return view("home");
+
+}
+public function services()
+{
+    return "Hello vishal coding master";
+}
+public function company()
+{
+    return "welcome to udaan";
+
+}
+public function contact_us()
+{
+    return "welcome to udaan company";
+
+}
+}
+
+
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\http\controllers\Basecontroller;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+route::get('/home',[Basecontroller::class,'home']);
+route::get('/services',[Basecontroller::class,'services']);
+route::get('/company',[Basecontroller::class,'company']);
+route::get('/contact_us',[Basecontroller::class,'contact_us']);
+
+route::post('/company',[Basecontroller::class,'company']);
+route::post('/company',[Basecontroller::class,'company']);
+route::post('/company',[Baseconteroller::class,'company']);
+
